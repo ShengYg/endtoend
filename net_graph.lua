3f546ba12c2b8285f204082355f676c8aeddb952
sy <shengy0726@163.com>
2016-06-14 17:35:18 +0800

    rgb image

--- net_graph.lua
+++ net_graph.lua
@@ -25,7 +25,7 @@ local function createModel()
 	cudnn.fastest = true
 	cudnn.benchmark = true
 
-	conv1 = Conv(2, 64, 3, 3, 2, 2, 1, 1)()
+	conv1 = Conv(6, 64, 3, 3, 2, 2, 1, 1)()
 	conv1a = BN(64)(conv1)
 	conv2 = BN(128)(Conv(64, 128, 3, 3, 2, 2, 1, 1)(ReLU(true)(conv1a)))
 	conv3a = BN(256)(Conv(128, 256, 3, 3, 2, 2, 1, 1)(ReLU(true)(conv2)))
@@ -38,30 +38,30 @@ local function createModel()
 	conv6b = BN(1024)(Conv(1024, 1024, 3, 3, 1, 1, 1, 1)(ReLU(true)(conv6a)))
 
 	upconv5 = BN(512)(DeConv(1024, 512, 4, 4, 2, 2, 1, 1)(ReLU(true)(conv6b)))
-	pr6 = BN(1)(Conv(1024, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(conv6b)))
+	pr6 = Conv(1024, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(conv6b))
 	pr6_1 = BN(1)(DeConv(1, 1, 4, 4, 2, 2, 1, 1)(ReLU(true)(pr6)))
 	iconv5 = BN(512)(Conv(1025, 512, 3, 3, 1, 1, 1, 1)(ReLU(true)((Join(2)({ReLU(true)(conv5b), ReLU(true)(pr6_1), ReLU(true)(upconv5)})))))
 
 	upconv4 = BN(256)(DeConv(512, 256, 4, 4, 2, 2, 1, 1)(ReLU(true)(iconv5)))
-	pr5 = BN(1)(Conv(512, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv5)))
+	pr5 = Conv(512, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv5))
 	pr5_1 = BN(1)(DeConv(1, 1, 4, 4, 2, 2, 1, 1)(ReLU(true)(pr5)))
 	iconv4 = BN(256)(Conv(769, 256, 3, 3, 1, 1, 1, 1)(ReLU(true)((Join(2)({ReLU(true)(conv4b), ReLU(true)(pr5_1), ReLU(true)(upconv4)})))))
 
 	upconv3 = BN(128)(DeConv(256, 128, 4, 4, 2, 2, 1, 1)(ReLU(true)(iconv4)))
-	pr4 = BN(1)(Conv(256, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv4)))
+	pr4 = Conv(256, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv4))
 	pr4_1 = BN(1)(DeConv(1, 1, 4, 4, 2, 2, 1, 1)(ReLU(true)(pr4)))
 	iconv3 = BN(128)(Conv(385, 128, 3, 3, 1, 1, 1, 1)(ReLU(true)((Join(2)({ReLU(true)(conv3b), ReLU(true)(pr4_1), ReLU(true)(upconv3)})))))
 
 	upconv2 = BN(64)(DeConv(128, 64, 4, 4, 2, 2, 1, 1)(ReLU(true)(iconv3)))
-	pr3 = BN(1)(Conv(128, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv3)))
+	pr3 = Conv(128, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv3))
 	pr3_1 = BN(1)(DeConv(1, 1, 4, 4, 2, 2, 1, 1)(ReLU(true)(pr3)))
 	iconv2 = BN(64)(Conv(193, 64, 3, 3, 1, 1, 1, 1)(ReLU(true)((Join(2)({ReLU(true)(conv2), ReLU(true)(pr3_1), ReLU(true)(upconv2)})))))
 
 	upconv1 = BN(32)(DeConv(64, 32, 4, 4, 2, 2, 1, 1)(ReLU(true)(iconv2)))
-	pr2 = BN(1)(Conv(64, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv2)))
+	pr2 = Conv(64, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv2))
 	pr2_1 = BN(1)(DeConv(1, 1, 4, 4, 2, 2, 1, 1)(ReLU(true)(pr2)))
 	iconv1 = BN(32)(Conv(97, 32, 3, 3, 1, 1, 1, 1)(ReLU(true)((Join(2)({ReLU(true)(conv1), ReLU(true)(pr2_1), ReLU(true)(upconv1)})))))
-	pr1 = BN(1)(Conv(32, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv1)))
+	pr1 = Conv(32, 1, 3, 3, 1, 1, 1, 1)(ReLU(true)(iconv1))
 
 	net = nn.gModule({conv1}, {pr1, pr2, pr3, pr4, pr5, pr6})
 	net:cuda()
